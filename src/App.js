7eb2c8057f13e081447113c7ad326f6073c40125
juditlehoczki <lehoczki.judit@gmail.com>
2020-03-06 16:59:47 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    currentWord: 'banana',
+    letters: [] // an array of letters already guessed (wrong and right ones too)
+  };
+
+  handleKeyPress = event => {
+    this.setState(currentState => {
+      if (!currentState.letters.includes(event.key)) {
+        return { letters: [...currentState.letters, event.key.toLowerCase()] };
+      }
+    });
+  };
+
+  showCurrentState = () => {
+    let currentState = '';
+    for (let i = 0; i < this.state.currentWord.length; i++) {
+      if (this.state.letters.includes(this.state.currentWord[i])) {
+        currentState = currentState.concat(this.state.currentWord[i]);
+      } else {
+        currentState = currentState.concat('_');
+      }
+      // console.log({ i }, { j });
+      // console.log(typeof this.state.letters[j]);
+    }
+    return currentState
+      .split('')
+      .join(' ')
+      .toUpperCase();
+  };
+
+  componentDidMount() {
+    document.addEventListener('keypress', this.handleKeyPress);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hang Man</h1>
+        <p>{this.showCurrentState()}</p>
+        <p>Tried Letters: {this.state.letters}</p>
+        <p>Guesses left: so and so</p>
+      </div>
+    );
+  }
 }
 
 export default App;
